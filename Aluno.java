c96ba850ec9a54d015e0dfff7175ab40f24d8395
Mariana dos Anjos <130617859+MarianaAnjo@users.noreply.github.com>
2024-03-05 08:46:09 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,17 @@
+package Escola;
+
+public class Aluno {
+
+    String nome;
+    int idade; 
+    String serie;
+
+    Aluno( String nome,
+            int idade,
+            String serie) {
+                this.nome = nome;
+                this.idade = idade;
+                this.serie = serie;
+            }
+    
+}
