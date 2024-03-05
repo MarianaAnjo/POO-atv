e5fe17210502e7b6584dc30c9131547fcb8b986c
Mariana dos Anjos <130617859+MarianaAnjo@users.noreply.github.com>
2024-03-05 08:45:05 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,15 @@
+package Carro;
+
+public class Carro {
+    String nome;
+    String cor;
+    double valor;
+
+     Carro (String nome,
+             String cor,
+             double valor) {
+                 this.nome = nome;
+                 this.cor = cor;
+                 this.valor = valor;
+                 }
+}
