65ff9a4e6c54b09affb11b7b4dbbbe388a293351
Mariana dos Anjos <130617859+MarianaAnjo@users.noreply.github.com>
2024-03-05 08:43:37 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,16 @@
+package Biblioteca;
+
+public class Livro {
+    String nome;
+    String autor;
+    int ano;
+
+    Livro( String nome,
+           String autor,
+           int ano){
+
+            this.nome = nome;
+            this.autor = autor;
+            this.ano = ano;
+           }
+}
